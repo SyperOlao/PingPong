d98a2f3b49c69b554583a4fa871eaba8c3158e47
SyperOlao <olay1234538@gmail.com>
2026-03-18 14:08:46 +0300

    bug fix

--- main.cpp
+++ main.cpp
@@ -8,13 +8,37 @@ int main()
 {
     try
     {
+        std::cout << "[LOG] main started\n";
+
         HINSTANCE hInstance = GetModuleHandleW(nullptr);
+        std::cout << "[LOG] got module handle\n";
+
         Application app(hInstance);
-        return app.Run();
+        std::cout << "[LOG] Application constructed\n";
+
+        const int result = app.Run();
+        std::cout << "[LOG] app.Run() finished with code: " << result << "\n";
+
+        return result;
     }
     catch (const std::exception& exception)
     {
-        MessageBoxA(nullptr, exception.what(), "Fatal Error", MB_OK | MB_ICONERROR);
+        const std::string message = std::string("Fatal error:\n") + exception.what();
+
+        std::cerr << message << std::endl;
+        OutputDebugStringA(message.c_str());
+        MessageBoxA(nullptr, message.c_str(), "Fatal Error", MB_OK | MB_ICONERROR);
+
+        return -1;
+    }
+    catch (...)
+    {
+        const char* message = "Unknown fatal error";
+
+        std::cerr << message << std::endl;
+        OutputDebugStringA(message);
+        MessageBoxA(nullptr, message, "Fatal Error", MB_OK | MB_ICONERROR);
+
         return -1;
     }
 }
